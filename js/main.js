a0fd7b3e67d68aa20728a93151d1b09d29329185
Servin Tolentino So <55864102+soservin07@users.noreply.github.com>
2020-06-13 20:47:53 -0700

    2nd Phase Aloha Update
    *finished Featured Category Menu, applied display GRID on it.
    *re-evaluate and re-organize sass template architecture
    *started to coding my first (.js) file
    *applied flickity on Most Feature Products carousel SLIDER.
    *added jsplugin for auto currency formatter
    *added "Add to Cart" button at flickity slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-(function($){
-
-    console.log('hello world');
-})(jQuery);
+$(document).ready(function()
+{
+     $('.product-price').formatCurrency();
+    $('.carousel-cell').append('<button class="btn-to-cart">Add to Cart</button>');
+});
